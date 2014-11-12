c4572ad091c0a31b0cc7f3a5cddb5f80f8b49a0e
Jorge Reis <jorgemiguelreis@gmail.com>
2014-11-12 20:25:57 +0000

    web app showing results

--- code/WebApp/main.js
+++ code/WebApp/main.js
@@ -1,133 +1,136 @@
+var searchphrase = "";
+
+//$(document).ready(function() {dbtest();});
+$(document).ready(function() {getArticleImage()});
 function search() {
+    $('#results_header').empty();
+    $('#results').empty();
+    
     var terms = $('#terms').val();
+    searchphrase = terms;
     if(terms) {
-        //alert(terms);
-        get();
-        //makeCorsRequest();
-        //httpGet('http://127.0.0.1:8983/solr/atracoes/select?q=title:castelo&wt=json&indent=true');
+        terms = terms.trim();
+        var words = terms.replace(" ", "+");
+        var url='http://localhost:8983/solr/atracoes/select?q=title:"'+words+'"&version=2.2&start=0&rows=1000&indent=on&wt=json&callback=?&json.wrf=on_data';
+        $.getJSON(url).fail(function(jqXHR, textStatus, errorThrown) {
+          if(textStatus != "parsererror") {
+           
+    var header = [
+        '<h1><strong class="text-danger">Solr Server not Available</strong></h1>',
+		'<h2 class="lead"> Request failed: net::ERR_CONNECTION_REFUSED</h2>'
+     ].join('\n');
+     
+        $('#results_header').append(header);
+          }   
+    });
     }
 }
 
-
-function get() {
- /*   $.get( "http://127.0.0.1:8983/solr/atracoes/select?q=title:castelo&wt=json&indent=true", function( data ) {
-        alert(data);
-        //$( ".result" ).html( data );
-       // alert( "Load was performed." );
-    });*/
-  /*  
-    $.ajax({
-        type:'GET',
-        url: "https://www.google.pt/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8",
-        data: {},
-        success: function(data) {
-            //console.log(data);
-            alert(data);
-  }, error: function(jqXHR, textStatus, errorThrown) {
-      alert(errorThrown);
-   //console.log(errorThrown);
-  }
-}); */
-    
-    /*
-    $.ajax({
-dataType: 'jsonp',
-data: 'id=10',
-jsonp: 'myCallbackFunction',
-url: 'http://localhost:8983/solr/atracoes/select?q=title:castelo&wt=json&indent=true',
-success: function () {
-    alert("AA");
-// do stuff
-},
-});
-*/
-    
-    
-        /*
-      var query = $('#query').val();
-        if (query.length == 0) {
-            return;
-        }
-        */
-
-        var url='http://localhost:8983/solr/atracoes/select?q=title:castelo&version=2.2&start=0&rows=50&indent=on&wt=json&callback=?&json.wrf=on_data';
-        $.getJSON(url);
-    
+function getArticleImage() {
+    var url = 'http://pt.wikipedia.org/w/api.php?action=query&titles='+'Casa_de_Serralves'+'&prop=pageimages&format=json&pithumbsize=100';
+    $.getJSON(url).done(function(data){alert(JSON.stringify(data));}) ;
+    alert("aquiiiiiii");
 }
+
+function on_data2(data) {
+    var docs = data.response.docs;
+    console.log(JSON.stringify(docs));
+}
+
+/*
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i=0, strLen=str.length; i<strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+
+function dbtest() {
+    var Uints = new Uint8Array(r.result);
+    db = new SQL.Database(Uints);
+    
+    var uInt8 = new Uint8Array(str2ab("atracoes.db"));
+    console.log(uInt8);
+    //Create the database
+    var db = new SQL.Database(uInt8);
+    // alert("QUI");
+    // Run a query without reading the results
+    //db.run("CREATE TABLE test (col1, col2);");
+    // Insert two rows: (1,111) and (2,222)
+    //db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
+   
+    // Prepare a statement
+    var stmt = db.prepare("SELECT * FROM category");
+    alert("QUI");
+    stmt.getAsObject(); // {col1:1, col2:111}
+
+    // Bind new values
+  //  stmt.bind();
+    while(stmt.step()) { //
+        var row = stmt.getAsObject();
+        alert(JSON.stringify(row));
+    }
+}*/
+
+/*
+$.ajaxSetup({
+      error : function(jqXHR, textStatus, errorThrown) {
+          if(textStatus != "parsererror") {
+           
+    var header = [
+        '<h1><strong class="text-danger">Solr Server not Available</strong></h1>',
+		'<h2 class="lead"> Request failed: net::ERR_CONNECTION_REFUSED</h2>'
+     ].join('\n');
+     
+        $('#results_header').append(header);
+          }   
+    }
+});*/
 
  function on_data(data) {
       $('#results').empty();
         var docs = data.response.docs;
+       // console.log(JSON.stringify(docs));
         $.each(docs, function(i, item) {
-            $('#results').prepend($('<div>' + item.title + '</div>'));
+            printResult(item);
         });
 
         var total = 'Found ' + docs.length + ' results';
-        $('#results').prepend('<div>' + total + '</div>');
-    }
-
-function myCallbackFunction(data){
-       // $('body').text(data.response);
-    alert(data);
-    }
-
-
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
+     
+     var header = [
+        '<h1>Search Results</h1>',
+		'<h2 class="lead"><strong class="text-danger">'+docs.length+'</strong> results were found for the search for <strong class="text-danger">'+searchphrase+'</strong></h2>'
+     ].join('\n');
+     
+        $('#results_header').append(header);
 }
 
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-  return text.match('<title>(.*)?</title>')[1];
-}
-
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // All HTML5 Rocks properties support CORS.
-  var url = 'http://127.0.0.1:8983/solr/atracoes/select?q=title:castelo&wt=json&indent=true';
-
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + url + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-}
-
-
-
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLH
-    ttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+function printResult(item) {
+    
+    var result_row = [
+        '<article class="search-result row">',
+			'<div class="col-xs-12 col-sm-12 col-md-3">',
+            '<a href="'+ item.url +'" title="'+item.title+'" class="thumbnail">',
+                '<img src="'+"http://lorempixel.com/250/140/people"+'" alt="'+item.title+'" /></a>',
+			'</div>',
+			'<div class="col-xs-12 col-sm-12 col-md-2">',
+				'<ul class="meta-search">',
+					'<li><i class="glyphicon glyphicon-calendar"></i> <span>'+item.pageid+'</span></li>',
+					'<li><i class="glyphicon glyphicon-time"></i> <span>'+item.categoryid+'</span></li>',
+					'<li><i class="glyphicon glyphicon-tags"></i> <span>'+item.lastchange.replace(
+    /^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})$/,
+    "$1/$2/$3 $4:$5:$6")+'</span></li>',
+				'</ul>',
+			'</div>',
+			'<div class="col-xs-12 col-sm-12 col-md-7 excerpet">',
+				'<h3><a href="'+item.url+'" title="">'+item.title+'</a></h3>',
+				'<p>'+item.first_paragraph+'</p>',
+			'</div>',
+			'<span class="clearfix borda"></span>',
+		'</article>"'
+                     ].join('\n');
+    
+    $('#results').append($(result_row));
 }
